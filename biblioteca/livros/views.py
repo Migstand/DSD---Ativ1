09c05c32fbf5fbb1734aea36379fa5699c33c26f
Migstand <mrsdahora@gmail.com>
2026-09-20 22:30:05 -0300

    Incio da primeira atividade de DSD

---
+++ biblioteca/livros/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Autor, Livro
+from .serializers import AutorSerializer, LivroSerializer
+
+class AutorViewSet(viewsets.ModelViewSet):
+	queryset = Autor.objects.all()
+
+	serializer_class = AutorSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class LivroViewSet(viewsets.ModelViewSet):
+	queryset = Livro.objects.all()
+	serializer_class = LivroSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
